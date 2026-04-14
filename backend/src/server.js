fbd5f0bdeb4cfb84357b46519c27e1d0df2c9219
Serhat Soruklu <84446211+SerhatSoruklu@users.noreply.github.com>
2026-04-14 11:34:27 +0100

    [serhat] bind backend HTTP before Mongo startup

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,13 +5,19 @@ const env = require('./config/env');
 const { connectMongo } = require('./config/mongoose');
 
 async function start() {
-  await connectMongo(env.mongoUri);
-
   app.listen(env.port, env.host, () => {
     process.stdout.write(
       `[chatpdm-backend] listening on http://${env.host}:${env.port} in ${env.nodeEnv}\n`,
     );
   });
+
+  try {
+    await connectMongo(env.mongoUri);
+  } catch (error) {
+    process.stderr.write(
+      `[chatpdm-backend] Mongo connection did not complete after server bind: ${error.message}\n`,
+    );
+  }
 }
 
 start().catch((error) => {
